52c753c732949065dd19c11da81f4aae4b757ef1
Alejandro III de Macedonia <alejandro@Alejandros-MacBook-Pro.local>
2020-09-26 21:30:17 -0500

    added ant and spider checks for enemies near

--- enemies/spider/Spider.gd
+++ enemies/spider/Spider.gd
@@ -1,1 +1,9 @@
 extends "res://enemies/Enemy.gd"
+
+func _on_ObjectiveCheckTimer_timeout():
+    var collider = $ObjectiveChecker.get_collider()
+    print("collider found ", collider)
+    if collider and collider.has_method("handle_spider_attack"):
+        collider.handle_spider_attack(self)
+
+
--- game_modes/Waves.gd
+++ game_modes/Waves.gd
@@ -3,6 +3,7 @@ extends Node
 
 var slingshot_class = preload("res://dragable_items/slingshot/Slingshot.tscn")
 var poison_class = preload("res://dragable_items/poison/Poison.tscn")
+var ant_class = preload("res://dragable_items/ant/Ant.tscn")
 
 func _on_Gui_item_draged(item_name: String, pos: Vector2):
 	if not is_in_dragable_area(pos):
@@ -17,6 +18,10 @@ func _on_Gui_item_draged(item_name: String, pos: Vector2):
 			var poison = poison_class.instance()
 			poison.init(pos)
 			$DraggableItems.spawn_item(poison)
+		"ant":
+			var ant = ant_class.instance()
+			ant.init(pos)
+			$DraggableItems.spawn_item(ant)
 
 func is_in_dragable_area(pos: Vector2):
 	var inside_x = pos.x >= $DragableAreaTL.position.x and pos.x < $DragableAreaBR.position.x
